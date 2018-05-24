c2068ae328923b1e2de23daf2604616ae89437cf
Braden <senst.b@gmail.com>
2018-05-24 06:04:43 -0700

    Updating AWS AMI in create script

--- create_instance.py
+++ create_instance.py
@@ -5,7 +5,7 @@ import boto3
 
 ec2 = boto3.resource('ec2')
 instance = ec2.create_instances(
-        ImageId = 'ami-1e299d7e',
+        ImageId = 'ami-6bc56f13',
         MinCount = 1,
         MaxCount = 1,
         InstanceType = 't2.micro')
