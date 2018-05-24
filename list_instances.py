cdaa41fb3c804e1c94ea3a70ffbc9b21bf845e0c
Braden <senst.b@gmail.com>
2018-05-24 06:08:36 -0700

    Fixing boto3 resource module import

--- list_instances.py
+++ list_instances.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import boto3
-ec2 = boto3.resources('ec2')
-for instances in ec2.instances.all():
+
+ec2 = boto3.resource('ec2')
+for instance in ec2.instances.all():
     print instance.id, instance.state
 
